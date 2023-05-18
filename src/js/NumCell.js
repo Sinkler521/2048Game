c610b7342a3ce96c8df73b105e12abb9bed8fce7
M M <Sinkler521@gmail.com>
2023-05-18 20:45:32 +0300

    18.05 changes modified functions for moving, added color change function and score calculating function

--- src/js/NumCell.js
+++ src/js/NumCell.js
@@ -12,9 +12,6 @@ class Tile{
         // calculating x and y both for visible part on webpage and for this object props
 
         let [randomX, randomY] = this.findUnique()
-        // CHECK IF THERE IS SUCH CELL AND RECALL RANDOM FUNCTION IF IT IS
-        //////////////////////////////////////////////////////////////
-
 
         newTile.style.setProperty('--shift-x', String(randomX))
         newTile.style.setProperty('--shift-y', String(randomY))
@@ -22,21 +19,24 @@ class Tile{
         newTile.y = randomY
 
         this.gridField.appendChild(newTile)
-        this.tilesList.push(newTile)
 
         if(canBeFour){
             let random = this.randNum(10)
             // 5 is just a random number. We need 10% chance to get 4 instead of 2 when the tile creating
             if(random === 5){
                 newTile.innerText = '4'
+                newTile.value = 4
             }
             else{
                 newTile.innerText = '2'
+                newTile.value = 2
             }
         }
         else{
             newTile.innerText = '2'
+            newTile.value = 2
         }
+        this.tilesList.push(newTile)
     }
 
     randNum(max){
@@ -71,118 +71,168 @@ class Tile{
         }
     }
 
+    addScore(cell){
+        let scoreTable = document.getElementById('score');
+        scoreTable.innerText = Number(scoreTable.innerText) + Number(cell.innerText)
+    }
+
     moveUp() {
-        // sort cells by y
         this.tilesList.sort((a, b) => a.y - b.y);
 
-        // move cells up
         for (let i = 0; i < this.tilesList.length; i++) {
-            let cell = this.tilesList[i];
-            let currentX = cell.x;
-            let currentY = cell.y;
+            const cell = this.tilesList[i];
+            const currentX = cell.x;
+            let newY = cell.y;
 
-            let newY = currentY;
+            for (let j = cell.y - 1; j >= 0; j--) {
+                const occupiedCell = this.tilesList.find(tile => tile.x === currentX && tile.y === j);
 
-            // Find nearest free position above the current cell
-            for (let j = currentY - 1; j >= 0; j--) {
-                let isOccupied = false;
-                for (let k = 0; k < this.tilesList.length; k++) {
-                    if (this.tilesList[k].x === currentX && this.tilesList[k].y === j) {
-                        isOccupied = true;
-                        break
-                    }
-                }
-                if (!isOccupied) {
+                if (!occupiedCell) {
                     newY = j;
+                } else if (occupiedCell.innerText === cell.innerText) {
+                    occupiedCell.innerText = String(Number(cell.innerText) * 2);
+                    this.addScore(occupiedCell)
+                    this.changeTileColor(occupiedCell)
+                    cell.remove();
+                    this.tilesList.splice(i, 1);
+                    i--;
+                    break;
                 }
             }
 
-            // Update cell coordinates and move it to the new position
-            cell.y = newY;
-            cell.style.setProperty('--shift-y', String(newY));
+            if (cell.y !== newY) {
+                cell.y = newY;
+                cell.style.setProperty('--shift-y', String(newY));
+            }
         }
     }
-    moveDown(){
+
+    moveDown() {
         this.tilesList.sort((a, b) => b.y - a.y);
 
         for (let i = 0; i < this.tilesList.length; i++) {
             const cell = this.tilesList[i];
             const currentX = cell.x;
-            const currentY = cell.y;
+            let newY = cell.y;
 
-            let newY = currentY;
+            for (let j = cell.y + 1; j < 4; j++) {
+                const occupiedCell = this.tilesList.find(tile => tile.x === currentX && tile.y === j);
 
-            for (let j = currentY + 1; j < 4; j++) {
-                let isOccupied = false;
-                for (let k = 0; k < this.tilesList.length; k++) {
-                    if (this.tilesList[k].x === currentX && this.tilesList[k].y === j) {
-                        isOccupied = true;
-                        break;
-                    }
-                }
-                if (!isOccupied) {
+                if (!occupiedCell) {
                     newY = j;
+                } else if (occupiedCell.innerText === cell.innerText) {
+                    occupiedCell.innerText = String(Number(cell.innerText) * 2);
+                    this.addScore(occupiedCell)
+                    this.changeTileColor(occupiedCell)
+                    cell.remove();
+                    this.tilesList.splice(i, 1);
+                    i--;
+                    break;
                 }
             }
 
-            cell.y = newY;
-            cell.style.setProperty('--shift-y', String(newY));
-        }
-    }
-
-    moveLeft(){
-        this.tilesList.sort((a, b) => b.x - a.x);
-
-        for (let i = 0; i < this.tilesList.length; i++) {
-            const cell = this.tilesList[i];
-            const currentX = cell.x;
-            const currentY = cell.y;
-
-            let newX = currentX;
-
-            for (let j = currentX - 1; j >= 0; j--) {
-                let isOccupied = false;
-                for (let k = 0; k < this.tilesList.length; k++) {
-                    if (this.tilesList[k].y === currentY && this.tilesList[k].x === j) {
-                        isOccupied = true;
-                        break;
-                    }
-                }
-                if (!isOccupied) {
-                    newX = j;
-                }
+            if (cell.y !== newY) {
+                cell.y = newY;
+                cell.style.setProperty('--shift-y', String(newY));
             }
-
-            cell.x = newX;
-            cell.style.setProperty('--shift-x', String(newX));
         }
     }
 
-    moveRight(){
+
+    moveLeft() {
         this.tilesList.sort((a, b) => a.x - b.x);
 
         for (let i = 0; i < this.tilesList.length; i++) {
             const cell = this.tilesList[i];
-            const currentX = cell.x;
             const currentY = cell.y;
+            let newX = cell.x;
 
-            let newX = currentX;
+            for (let j = cell.x - 1; j >= 0; j--) {
+                const occupiedCell = this.tilesList.find(tile => tile.x === j && tile.y === currentY);
 
-            for (let j = currentX + 1; j < 4; j++) {
-                let isOccupied = false;
-                for (let k = 0; k < this.tilesList.length; k++) {
-                    if (this.tilesList[k].y === currentY && this.tilesList[k].x === j) {
-                        isOccupied = true;
-                        break;
-                    }
-                }
-                if (!isOccupied) {
+                if (!occupiedCell) {
                     newX = j;
+                } else if (occupiedCell.innerText === cell.innerText) {
+                    occupiedCell.innerText = String(Number(cell.innerText) * 2);
+                    this.addScore(occupiedCell)
+                    this.changeTileColor(occupiedCell)
+                    cell.remove();
+                    this.tilesList.splice(i, 1);
+                    i--;
+                    break;
                 }
             }
 
-            cell.x = newX;
-            cell.style.setProperty('--shift-x', String(newX));
+            if (cell.x !== newX) {
+                cell.x = newX;
+                cell.style.setProperty('--shift-x', String(newX));
+            }
+        }
+    }
+
+    moveRight() {
+        this.tilesList.sort((a, b) => b.x - a.x);
+
+        for (let i = 0; i < this.tilesList.length; i++) {
+            const cell = this.tilesList[i];
+            const currentY = cell.y;
+            let newX = cell.x;
+
+            for (let j = cell.x + 1; j < 4; j++) {
+                const occupiedCell = this.tilesList.find(tile => tile.x === j && tile.y === currentY);
+
+                if (!occupiedCell) {
+                    newX = j;
+                } else if (occupiedCell.innerText === cell.innerText) {
+                    occupiedCell.innerText = String(Number(cell.innerText) * 2);
+                    this.addScore(occupiedCell)
+                    this.changeTileColor(occupiedCell)
+                    cell.remove();
+                    this.tilesList.splice(i, 1);
+                    i--;
+                    break;
+                }
+            }
+
+            if (cell.x !== newX) {
+                cell.x = newX;
+                cell.style.setProperty('--shift-x', String(newX));
+            }
+        }
+    }
+
+    changeTileColor(tile){
+
+        switch (tile.innerText){
+            case '4':
+                tile.style.backgroundColor = '#dadce3';
+                break;
+            case '8':
+                tile.style.backgroundColor = '#cbd2f3';
+                break;
+            case '16':
+                tile.style.backgroundColor = '#b3bef3';
+                break;
+            case '32':
+                tile.style.backgroundColor = '#9eacef';
+                break;
+            case '64':
+                tile.style.backgroundColor = '#98a0b8';
+                break;
+            case '128':
+                tile.style.backgroundColor = '#8994b3';
+                break;
+            case '256':
+                tile.style.backgroundColor = '#8fbdaa';
+                break;
+            case '512':
+                tile.style.backgroundColor = '#68b394';
+                break;
+            case '1028':
+                tile.style.backgroundColor = '#4ba37f';
+                break;
+            case '2048':
+                tile.style.backgroundColor = '#218f62'
         }
     }
 }
